0f550d8d8f8367b6e770cec370e3b24576ade905
Human CTA <it@humancta.org>
2024-10-13 01:35:41 +0200

    Update installacio-odoo_v16-postgres_v14.sh

--- installacio-odoo_v16-postgres_v14.sh
+++ installacio-odoo_v16-postgres_v14.sh
@@ -174,7 +174,7 @@ selected_server_tools=()
 
 # Pregunta si es volen instal·lar tots els mòduls per defecte
 
-read -p "Vols instal·lar tots els mòduls per defecte? (s/n): (s)"  install_all_modules
+read -p "Vols instal·lar tots els mòduls per defecte? (s/n): (s) " install_all_modules
 install_all_modules=${install_all_modules:-s}
 
 if [[ "$install_all_modules" == "s" || "$install_all_modules" == "S" ]]; then
@@ -199,7 +199,7 @@ else
 fi
 
 # Pregunta si es volen instal·lar tots els Server Tools per defecte
-read -p "Vols instal·lar tots els Server Tools per defecte? (s/n): (s)"  install_all_tools
+read -p "Vols instal·lar tots els Server Tools per defecte? (s/n): (s) " install_all_tools
 install_all_tools=${install_all_tools:-s}
 
 if [[ "$install_all_tools" == "s" || "$install_all_tools" == "S" ]]; then
