d4c7c94d5833a8bb7ca4702016f8919a3f194ae7
Human CTA <it@humancta.org>
2024-10-15 18:27:23 +0200

    Update installacio-odoo_v16-postgres_v14.sh

--- installacio-odoo_v16-postgres_v14.sh
+++ installacio-odoo_v16-postgres_v14.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Funció per generar una contrasenya aleatòria de 16 caràcters
 function generate_random_password {
   echo "$(tr -dc 'A-Za-z0-9?¿¡!@#$%^&*()_+=-' < /dev/urandom | head -c 16)"
@@ -18,7 +20,6 @@ function prompt_required {
   echo ${input_value:-$default_value}
 }
 
-
 # Funció per demanar dades amb validació "s/n", amb resposta per defecte a "s"
 function prompt_yes_no {
   local prompt_text=$1
@@ -33,14 +34,13 @@ function prompt_yes_no {
 }
 
 # Demanar el nom de la instància abans de tot
-echo "Introduïu les dades del voatre Odoo"
+echo -e "\e[1m\e[34mIntroduïu les dades del vostre Odoo\e[0m"
 echo 
 instance_name=$(prompt_required "Introdueix el nom de la instància de Lightsail")
 
 # Generar valors per defecte per la base de dades i l'usuari basats en el nom de la instància
 db_name_default=$(echo "${instance_name}_db" | tr '-' '_')
 db_user_default=$(echo "${instance_name}_user" | tr '-' '_')
-
 
 # Generar contrasenyes aleatòries per defecte
 master_password_default=$(generate_random_password)
@@ -52,21 +52,21 @@ static_ip=$(prompt_required "Introdueix la IP estàtica de la instància")
 
 # Generar el valor per defecte del domini dinàmicament
 custom_domain_default="intranet.${instance_name}.com"
-
 custom_domain=$(prompt_required "Introdueix el nom de domini" "$custom_domain_default")
+
 master_password=$(prompt_required "Introdueix la contrasenya Màster" "$master_password_default")
 db_name=$(prompt_required "Introdueix el nom de la base de dades" "$db_name_default")
 db_user=$(prompt_required "Introdueix el nom d'usuari de la base de dades" "$db_user_default")
 db_password=$(prompt_required "Introdueix la contrasenya de l'usuari de la base de dades" "$db_password_default")
-admin_email=$(prompt_required "Introdueix el correu electrònic de l'administrador" "it@humancta.org") #correu per defecte it@humancta.org
+admin_email=$(prompt_required "Introdueix el correu electrònic de l'administrador" "it@humancta.org")
 admin_password=$(prompt_required "Introdueix la contrasenya de l'administrador" "$admin_password_default")
 
 # Demanar idioma i país amb valors per defecte
-admin_language=$(prompt_required "Introdueix l'idioma" "Català")  # Idioma per defecte Català
-admin_country=$(prompt_required "Introdueix el país" "Spain")     # País per defecte Spain
+admin_language=$(prompt_required "Introdueix l'idioma" "Català")
+admin_country=$(prompt_required "Introdueix el país" "Spain")
 
 echo 
-install_demo_data=$(prompt_yes_no "Vols instal·lar dades de mostra? (s/n)" "n")  # Dades de mostra per defecte NO
+install_demo_data=$(prompt_yes_no "Vols instal·lar dades de mostra? (s/n)" "n")
 
 # Convertir la resposta de "s" o "n" en booleà per la configuració
 if [[ "$install_demo_data" == "s" || "$install_demo_data" == "S" ]]; then
@@ -128,14 +128,12 @@ read -p 'Vols instal·lar tots els mòduls per defecte? (s/n): (s) ' install_all
 install_all_modules=${install_all_modules:-s}
 
 if [[ "$install_all_modules" == "s" || "$install_all_modules" == "S" ]]; then
-  # Si l'usuari diu 's', instal·la tots els mòduls per defecte
-    echo "Tots els mòduls predeterminats seleccionats"
+  echo "Tots els mòduls predeterminats seleccionats"
   for module in "${default_modules[@]}"; do
     module_name=$(echo $module | cut -d' ' -f1)
     selected_default_modules+=("$module_name")
   done
 else
-  # Si l'usuari diu 'n', mostrar un per un
   echo "Selecciona els mòduls d'Odoo que vols instal·lar (s/n, 's' per defecte per a tots):"
   for module in "${default_modules[@]}"; do
     read -p "$module (s/n): (s) " choice
@@ -154,14 +152,12 @@ read -p 'Vols instal·lar tots els Server Tools per defecte? (s/n): (s) ' instal
 install_all_tools=${install_all_tools:-s}
 
 if [[ "$install_all_tools" == "s" || "$install_all_tools" == "S" ]]; then
-  # Si l'usuari diu 's', instal·la tots els Server Tools per defecte
-    echo "Tots els Server Tools predeterminats seleccionats"
+  echo "Tots els Server Tools predeterminats seleccionats"
   for tool in "${server_tools_modules[@]}"; do
     tool_name=$(echo $tool | cut -d' ' -f1)
     selected_server_tools+=("$tool_name")
   done
 else
-  # Si l'usuari diu 'n', mostrar un per un
   echo "Selecciona els Server Tools que vols instal·lar (s/n, 's' per defecte per a tots):"
   for tool in "${server_tools_modules[@]}"; do
     read -p "$tool (s/n): (s) " choice
@@ -175,18 +171,17 @@ else
 fi
 
 # Confirmació final dels mòduls seleccionats
-echo 
-echo "Mòduls seleccionats per a la instal·lació:"
+echo -e "\e[1m\e[34mMòduls seleccionats per a la instal·lació:\e[0m"
 for module in "${selected_default_modules[@]}"; do
   echo "- $module"
 done
 
-echo 
-echo "Server Tools seleccionats per a la instal·lació:"
+echo -e "\e[1m\e[34mServer Tools seleccionats per a la instal·lació:\e[0m"
 for tool in "${selected_server_tools[@]}"; do
   echo "- $tool"
 done
 
+# Confirmar abans de continuar
 echo 
 read -p 'Vols continuar amb aquests mòduls seleccionats? (s/n) (s): ' confirm_modules
 confirm_modules=${confirm_modules:-s}
@@ -197,7 +192,7 @@ fi
 
 # Mostrar els valors seleccionats
 function mostrar_valors {
-  echo "Configuració seleccionada:"
+  echo -e "\e[1m\e[33mConfiguració seleccionada:\e[0m"
   echo "  Nom de la instància de Lightsail: $instance_name"
   echo "  IP estàtica de la instància: $static_ip"
   echo "  Nom de domini: $custom_domain"
@@ -217,7 +212,6 @@ function mostrar_valors {
 # Confirmar els valors abans de continuar
 mostrar_valors
 
-echo
 read -p 'Vols continuar la instal·lació amb aquests valors? (s/n): (s) ' confirm
 confirm=${confirm:-s}  # Si l'usuari no introdueix res, assigna 's' per defecte
 
@@ -227,68 +221,57 @@ if [[ $confirm != "s" ]]; then
 fi
 
 # Actualitzar el servidor
-echo 
-echo "Actualitzant el servidor..."
+echo -e "\e[1m\e[34mActualitzant el servidor...\e[0m"
 sudo apt update -y && sudo apt upgrade -y
 
 # Instal·lació de seguretat SSH i Fail2ban
-echo 
-echo "Instal·lant seguretat SSH i Fail2ban..."
+echo -e "\e[1m\e[34mInstal·lant seguretat SSH i Fail2ban...\e[0m"
 sudo apt-get install openssh-server fail2ban -y
 
 # Instal·lació de llibreries necessàries
-echo 
-echo "Instal·lant llibreries necessàries..."
+echo -e "\e[1m\e[34mInstal·lant llibreries necessàries...\e[0m"
 sudo apt install vim curl wget gpg git gnupg2 software-properties-common apt-transport-https lsb-release ca-certificates -y
 sudo apt install build-essential wget git python3 python3-pip python3-dev python3-venv python3-wheel libfreetype6-dev libxml2-dev libzip-dev libsasl2-dev python3-setuptools libjpeg-dev zlib1g-dev libpq-dev libxslt1-dev libldap2-dev libtiff5-dev libopenjp2-7-dev -y
 
 # Instal·lació de Node.js i NPM
-echo 
-echo "Instal·lant Node.js i NPM..."
+echo -e "\e[1m\e[34mInstal·lant Node.js i NPM...\e[0m"
 sudo apt install nodejs npm node-less xfonts-75dpi xfonts-base fontconfig -y
 sudo npm install -g rtlcss
 
 # Instal·lació de Wkhtmltopdf
-echo 
-echo "Instal·lant Wkhtmltopdf..."
+echo -e "\e[1m\e[34mInstal·lant Wkhtmltopdf...\e[0m"
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb
 sudo dpkg -i wkhtmltox_0.12.6.1-2.jammy_amd64.deb
 sudo apt-get install -f -y
 
 # Instal·lació de PostgreSQL 14
-echo 
-echo "Instal·lant PostgreSQL 14..."
+echo -e "\e[1m\e[34mInstal·lant PostgreSQL 14...\e[0m"
 curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
 echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
 sudo apt update
 sudo apt -y install postgresql-14 postgresql-client-14
 
 # Creació de la base de dades i usuari PostgreSQL per Odoo
-echo 
-echo "Creant base de dades i usuari PostgreSQL per Odoo..."
+echo -e "\e[1m\e[34mCreant base de dades i usuari PostgreSQL per Odoo...\e[0m"
 sudo su - postgres -c "psql -c \"CREATE DATABASE $db_name;\""
 sudo su - postgres -c "createuser -p 5432 -s $db_user"
 sudo su - postgres -c "psql -c \"ALTER USER $db_user WITH PASSWORD '$db_password';\""
 
 # Configurar autenticació PostgreSQL
-echo 
-echo "Configurant autenticació PostgreSQL..."
+echo -e "\e[1m\e[34mConfigurant autenticació PostgreSQL...\e[0m"
 sudo bash -c "echo 'local   all             all                                     md5' >> /etc/postgresql/14/main/pg_hba.conf"
 sudo systemctl restart postgresql
 
 # Creació de l'usuari Odoo
-echo 
-echo "Creant usuari Odoo al sistema..."
+echo -e "\e[1m\e[34mCreant usuari Odoo al sistema...\e[0m"
 sudo adduser --system --group --home=/opt/odoo --shell=/bin/bash odoo
 
 # Clonar el repositori Odoo 16
-echo 
-echo "Clonant el repositori Odoo 16..."
+echo -e "\e[1m\e[34mClonant el repositori Odoo 16...\e[0m"
 sudo su - odoo -c "git clone https://github.com/odoo/odoo.git --depth 1 --branch 16.0 --single-branch /opt/odoo/odoo-server"
 
 # Crear entorn virtual de Python
-echo 
-echo "Creant entorn virtual de Python..."
+echo -e "\e[1m\e[34mCreant entorn virtual de Python...\e[0m"
 sudo su - odoo -c "python3 -m venv /opt/odoo/odoo-server/venv"
 sudo su - odoo -c "/opt/odoo/odoo-server/venv/bin/pip install wheel"
 sudo su - odoo -c "/opt/odoo/odoo-server/venv/bin/pip install -r /opt/odoo/odoo-server/requirements.txt"
@@ -300,7 +283,6 @@ install_module() {
   local dest_dir=$3
   local type=$4
 
-  # Crear la carpeta de destinació
   sudo mkdir -p "$dest_dir/$name"
 
   if [ ! -d "$dest_dir/$name" ]; then
@@ -327,24 +309,15 @@ for tool in "${selected_server_tools[@]}"; do
   install_module "$tool" "/opt/odoo/odoo-server/odoo/server-tools" "/opt/odoo/odoo-server/server-tools" "Server Tool"
 done
 
-# echo 
-# Exemple per clonar un mòdul de tercers dins custom_addons:
-# Substitueix "third_party_module" pel nom del mòdul de tercers que vulguis clonar.
-# sudo mkdir -p /opt/odoo/odoo-server/custom_addons/third_party_module
-# sudo su - odoo -c "git clone https://github.com/third_party_repo/third_party_module.git /opt/odoo/odoo-server/custom_addons/third_party_module"
-# Nota: Substitueix "third_party_module" pel nom correcte del mòdul de tercers.
-
 # Crear directori de logs
-echo 
-echo "Creant directori de logs..."
+echo -e "\e[1m\e[34mCreant directori de logs...\e[0m"
 sudo mkdir /var/log/odoo
 sudo touch /var/log/odoo/odoo-server.log
 sudo chown odoo:odoo /var/log/odoo -R
 sudo chmod 777 /var/log/odoo
 
 # Crear fitxer de configuració d'Odoo
-echo 
-echo "Creant fitxer de configuració d'Odoo..."
+echo -e "\e[1m\e[34mCreant fitxer de configuració d'Odoo...\e[0m"
 sudo bash -c "cat > /etc/odoo.conf" <<EOL
 [options]
 instance_name = $instance_name
@@ -368,8 +341,7 @@ EOL
 sudo chown odoo:odoo /etc/odoo.conf
 
 # Crear servei d'Odoo
-echo 
-echo "Creant servei d'Odoo..."
+echo -e "\e[1m\e[34mCreant servei d'Odoo...\e[0m"
 sudo bash -c "cat > /etc/systemd/system/odoo-server.service" <<EOL
 [Unit]
 Description=Odoo Service
@@ -390,20 +362,17 @@ WantedBy=multi-user.target
 EOL
 
 # Iniciar i habilitar el servei
-echo 
-echo "Iniciant i habilitant el servei d'Odoo..."
+echo -e "\e[1m\e[34mIniciant i habilitant el servei d'Odoo...\e[0m"
 sudo systemctl daemon-reload
 sudo systemctl start odoo-server
 sudo systemctl enable odoo-server
 
 # Instal·lació de Nginx
-echo 
-echo "Instal·lant Nginx..."
+echo -e "\e[1m\e[34mInstal·lant Nginx...\e[0m"
 sudo apt install nginx -y
 
 # Configuració de Nginx
-echo 
-echo "Configurant Nginx per Odoo..."
+echo -e "\e[1m\e[34mConfigurant Nginx per Odoo...\e[0m"
 sudo bash -c "cat > /etc/nginx/sites-available/$custom_domain" <<EOL
 upstream odoo16 {
     server 127.0.0.1:8069;
@@ -427,12 +396,12 @@ server {
 EOL
 
 # Activar configuració Nginx
-echo 
-echo "Activant configuració Nginx..."
+echo -e "\e[1m\e[34mActivant configuració Nginx...\e[0m"
 sudo ln -s /etc/nginx/sites-available/$custom_domain /etc/nginx/sites-enabled/
 sudo nginx -t
 sudo systemctl restart nginx
 
+echo -e "\e[1m\e[34mEliminant arxius d'instal·lació...\e[0m"
 # Esborrar els fitxers .deb baixats
 sudo rm -f /home/ubuntu/*.deb
 sudo rm -f /home/ubuntu/*.deb.*
@@ -451,7 +420,7 @@ echo "Fitxers temporals eliminats."
 # Mostrar les variables i el missatge final
 mostrar_valors
 echo 
-echo "Instal·lació d'Odoo completada correctament!"
+echo -e "\e[1m\e[34mInstal·lació acabada correctament!\e[0m"
 echo
 echo "Creeu el següent registre al Keeweb:"
 echo "Nom del registre: $instance_name"
